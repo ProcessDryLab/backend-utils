85313257eb5ad58817b93572acf4f94dcf4190a9
Andrea Burattin <andrea.burattin@gmail.com>
2022-11-08 12:01:54 +0100

    Added constructor with no parameters

--- src/main/java/pdl/web/models/MinerConfiguration.java
+++ src/main/java/pdl/web/models/MinerConfiguration.java
@@ -9,11 +9,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class MinerConfiguration {
 
 	private Miner miner = null;
